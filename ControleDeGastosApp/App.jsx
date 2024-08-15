b057d0ad056d3abb7fdfb53492665e8f776675e5
Jhonatan <jhonatanwcom@gmail.com>
2024-08-15 15:10:37 -0300

    novo layout telainserir dados

--- ControleDeGastosApp/App.jsx
+++ ControleDeGastosApp/App.jsx
@@ -18,7 +18,7 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName="InserirDados">
+      <Stack.Navigator initialRouteName="Dashboard">
         <Stack.Screen name="Dashboard" component={TelaDashboard} />
         <Stack.Screen name="InserirDados" component={TelaInserirDados} />
         <Stack.Screen name="GastosMes" component={TelaListaGastos} />
--- ControleDeGastosApp/screens/TelaInserirDados.jsx
+++ ControleDeGastosApp/screens/TelaInserirDados.jsx
@@ -1,28 +1,39 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  View,
+  TextInput,
+  Button,
+  Text,
+  Switch,
+  StyleSheet,
+} from "react-native";
 
 const TelaInserirDados = ({ styleText }) => {
   const [quemPagou, setQuemPagou] = useState(null);
-  const [valorGasto, setValorGasto] = useState("");
+  const [valorPago, setValorPago] = useState("");
   const [comoDividirValor, setComoDividirValor] = useState(null);
-  const [tipoGasto, setTipoGasto] = useState(null);
+  const [gastoOuGanho, setGastoOuGanho] = useState(null);
   const [data, setData] = useState("");
   const [descricao, setDescricao] = useState("");
 
+  const handleSave = () => {
+    // Lógica para salvar os dados
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>TELA TÍTULO: TELAINSERIRDADOS</Text>
+      <Text>TELA TÍTULO: TELAINSERIRDADOS</Text>
 
-      <Text style={styles.textStyle}>VALOR GASTO</Text>
+      <Text>VALOR PAGO:</Text>
       <TextInput
         style={styles.input}
-        placeholder="Digite o valor"
+        placeholder="Digite o valor PAGO"
         keyboardType="numeric"
-        value={valorGasto}
-        onChangeText={setValorGasto}
+        value={valorPago}
+        onChangeText={setValorPago}
       />
-
-      <Text style={styles.textStyle}>QUEM PAGOU?</Text>
+      <Text>QUEM PAGOU?</Text>
       <View style={styles.buttonGroup}>
         <Button
           title="Nome1"
@@ -36,47 +47,35 @@ const TelaInserirDados = ({ styleText }) => {
         />
       </View>
 
-      <Text style={styles.textStyle}>COMO FOI PAGO?</Text>
-      <View style={styles.buttonGroup}>
-        <Button
-          title="Porcentagem"
-          onPress={() => setComoDividirValor("porcentagem")}
-          color={comoDividirValor === "porcentagem" ? "blue" : "gray"}
-        />
-        <Button
-          title="Dividido Igualmente"
-          onPress={() => setComoDividirValor("igualmente")}
-          color={comoDividirValor === "igualmente" ? "blue" : "gray"}
-        />
+      <View style={styles.switchContainer}>
+        <Text>Gasto</Text>
+        <Switch value={gastoOuGanho} onValueChange={setGastoOuGanho} />
+        <Text>Ganho</Text>
       </View>
 
-      <Text style={styles.textStyle}>É UM GANHO OU GASTO?</Text>
-      <View style={styles.buttonGroup}>
-        <Button
-          title="Ganho"
-          onPress={() => setTipoGasto("Ganho")}
-          color={tipoGasto === "Ganho" ? "blue" : "gray"}
-        />
-        <Button
-          title="Gasto"
-          onPress={() => setTipoGasto("Gasto")}
-          color={tipoGasto === "Gasto" ? "blue" : "gray"}
-        />
+      <Text>COMO SERÁ DIVIDIDO:</Text>
+      <View style={styles.switchContainer}>
+        <Text>Igualmente</Text>
+        <Switch value={comoDividirValor} onValueChange={setComoDividirValor} />
+        <Text>Por Porcentagem</Text>
       </View>
-      <Text style={styles.textStyle}>DATA:</Text>
+
+      <Text>DATA:</Text>
       <TextInput
         style={styles.input}
         placeholder="Digite a data"
         value={data}
         onChangeText={setData}
       />
-      <Text style={styles.textStyle}>DESCRIÇÃO:</Text>
+      <Text>DESCRIÇÃO:</Text>
       <TextInput
         style={styles.input}
         placeholder="Escreva a descrição"
         value={descricao}
         onChangeText={setDescricao}
       />
+
+      <Button title="Salvar" onPress={handleSave} />
     </View>
   );
 };
@@ -88,16 +87,17 @@ const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
     padding: 10,
-    borderRadius: 10,
+    // borderRadius: 10,
     marginVertical: 10,
   },
   switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
-buttonGroup: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  buttonGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 });
 
